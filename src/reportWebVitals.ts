ad8eaac25092356eddc430c52db795c0b7450e2c
maykosh <reacttsdev@gmail.com>
2025-02-03 17:30:31 +0500

    reportWebVitals in ts setupTests delete

---
+++ src/reportWebVitals.ts
@@ -0,0 +1,16 @@
+import { ReportHandler } from "web-vitals";
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
+   if (onPerfEntry && onPerfEntry instanceof Function) {
+      import("web-vitals").then(
+         ({ getCLS, getFID, getLCP, getFCP, getTTFB }) => {
+            getCLS(onPerfEntry);
+            getFID(onPerfEntry);
+            getLCP(onPerfEntry);
+            getFCP(onPerfEntry);
+            getTTFB(onPerfEntry);
+         }
+      );
+   }
+};
+
+export default reportWebVitals;
